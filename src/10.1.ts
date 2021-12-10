7f9e446358f63ffbf884f270983b01a5c914d7d2
Bas Dirks <bas.dirks@bettyblocks.com>
2021-12-10 06:35:42 +0100

    day 10

---
+++ src/10.1.ts
@@ -0,0 +1,60 @@
+import { readFile } from 'fs/promises';
+
+const solve = (input: string): number => {
+  const lines = input
+    .trim()
+    .split('\n')
+    .map((line: string): string[] => [...line]);
+
+  return lines.reduce((score: number, line: string[]): number => {
+    const open = [];
+
+    for (let i = 0; i < line.length; i += 1) {
+      const char = line[i]!;
+
+      if ('([{<'.includes(char)) {
+        open.push(char);
+      } else {
+        const expected = {
+          '(': ')',
+          '[': ']',
+          '{': '}',
+          '<': '>',
+        }[open[open.length - 1]!];
+
+        if (char === expected) {
+          open.pop();
+        } else {
+          return (
+            score +
+            {
+              ')': 3,
+              ']': 57,
+              '}': 1197,
+              '>': 25137,
+            }[char]!
+          );
+        }
+      }
+    }
+
+    return score;
+  }, 0);
+};
+
+(async (): Promise<void> => {
+  const test = `
+[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]
+  `.trim();
+  console.log(solve(await readFile('./input/10', 'utf-8')));
+  console.log(solve(test));
+})();
---
+++ src/10.2.ts
@@ -0,0 +1,59 @@
+import { readFile } from 'fs/promises';
+
+const solve = (input: string): number => {
+  const lines = input
+    .trim()
+    .split('\n')
+    .map((line: string): string[] => [...line]);
+
+  const scores = lines.reduce((scores: number[], line: string[]): number[] => {
+    const open = [];
+
+    for (let i = 0; i < line.length; i += 1) {
+      const char = line[i]!;
+
+      if ('([{<'.includes(char)) {
+        open.push(char);
+      } else {
+        const expected = {
+          '(': ')',
+          '[': ']',
+          '{': '}',
+          '<': '>',
+        }[open[open.length - 1]!];
+
+        if (char === expected) {
+          open.pop();
+        } else {
+          return scores;
+        }
+      }
+    }
+
+    scores.push(
+      open.reverse().reduce(
+        (lineScore: number, char: string): number =>
+          lineScore * 5 +
+          {
+            '(': 1,
+            '[': 2,
+            '{': 3,
+            '<': 4,
+          }[char]!,
+        0
+      )
+    );
+
+    return scores;
+  }, []);
+
+  console.log(scores);
+
+  return scores.sort((a: number, b: number): number => a - b)[
+    Math.round(scores.length / 2 - 1)
+  ]!;
+};
+
+(async (): Promise<void> => {
+  console.log(solve(await readFile('./input/10', 'utf-8')));
+})();
