ea543e78e333790eb58e94a2a8b1eb70d8218d25
Bas Dirks <bas.dirks@bettyblocks.com>
2021-12-17 06:58:00 +0100

    clean up day 17

--- src/17.1.ts
+++ src/17.1.ts
@@ -1,42 +1,32 @@
-const hitMaxHeight = (
-  xVel: number,
-  yVel: number,
-  xMin: number,
-  xMax: number,
-  yMin: number,
-  yMax: number
-): number | null => {
-  let x = 0;
-  let y = 0;
-  let highest = Number.NEGATIVE_INFINITY;
-
-  while (true) {
-    x += xVel;
-    y += yVel;
-    if (xVel > 0) xVel -= 1;
-    if (xVel < 0) xVel += 1;
-    yVel -= 1;
-
-    if (y > highest) highest = y;
-    if (yVel < 0 && (x > xMax || y < yMax)) return null;
-    if (x > xMin && y < yMin) return highest;
-  }
-};
-
-(async (): Promise<void> => {
+((): void => {
   const xMin = 138;
   const xMax = 184;
   const yMin = -71;
   const yMax = -125;
+
+  const hitMaxHeight = (xVel: number, yVel: number): number => {
+    let x = 0;
+    let y = 0;
+    let highest = Number.NEGATIVE_INFINITY;
+
+    while (true) {
+      x += xVel;
+      y += yVel;
+      if (xVel > 0) xVel -= 1;
+      if (xVel < 0) xVel += 1;
+      yVel -= 1;
+
+      if (y > highest) highest = y;
+      if (yVel < 0 && (x > xMax || y < yMax)) return Number.NEGATIVE_INFINITY;
+      if (x > xMin && y < yMin) return highest;
+    }
+  };
+
   let maxTotal = 0;
 
-  for (let xVel = 1; xVel < xMax; xVel += 1) {
-    for (let yVel = 1; yVel < yMax; yVel += 1) {
-      const max = hitMaxHeight(xVel, yVel, xMin, xMax, yMin, yMax);
-
-      if (max !== null && max > maxTotal) maxTotal = max;
-    }
-  }
+  for (let xVel = 1; xVel <= xMax; xVel += 1)
+    for (let yVel = yMax; yVel <= -yMax; yVel += 1)
+      maxTotal = Math.max(maxTotal, hitMaxHeight(xVel, yVel));
 
   console.log(maxTotal);
 })();
--- src/17.2.ts
+++ src/17.2.ts
@@ -1,36 +1,30 @@
-const isHit = (
-  xVel: number,
-  yVel: number,
-  xMin: number,
-  xMax: number,
-  yMin: number,
-  yMax: number
-): boolean => {
-  let x = 0;
-  let y = 0;
-
-  while (true) {
-    x += xVel;
-    y += yVel;
-    if (xVel > 0) xVel -= 1;
-    if (xVel < 0) xVel += 1;
-    yVel -= 1;
-
-    if (yVel < 0 && (x > xMax || y < yMax)) return false;
-    if (x >= xMin && y <= yMin) return true;
-  }
-};
-
-(async (): Promise<void> => {
+((): void => {
   const xMin = 138;
   const xMax = 184;
   const yMin = -71;
   const yMax = -125;
+
+  const isHit = (xVel: number, yVel: number): boolean => {
+    let x = 0;
+    let y = 0;
+
+    while (true) {
+      x += xVel;
+      y += yVel;
+      if (xVel > 0) xVel -= 1;
+      if (xVel < 0) xVel += 1;
+      yVel -= 1;
+
+      if (yVel < 0 && (x > xMax || y < yMax)) return false;
+      if (x >= xMin && y <= yMin) return true;
+    }
+  };
+
   let n = 0;
 
   for (let xVel = 1; xVel <= xMax; xVel += 1)
     for (let yVel = yMax; yVel <= -yMax; yVel += 1)
-      n += isHit(xVel, yVel, xMin, xMax, yMin, yMax) ? 1 : 0;
+      n += isHit(xVel, yVel) ? 1 : 0;
 
   console.log(n);
 })();
