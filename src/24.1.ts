229f7d4bce39f2d360deee703bab857efd086124
Bas Dirks <bas.dirks@bettyblocks.com>
2021-12-25 07:41:07 +0100

    Day 24

--- src/24.1.ts
+++ src/24.1.ts
@@ -1,1 +1,20 @@
-// :)
+// ✎
+
+((): void => {
+  const a = 4;
+  const b = 1;
+  const c = 8;
+  const d = 1;
+  const e = 1;
+  const f = 7;
+  const g = 6;
+  const h = 1;
+  const i = 1;
+  const j = 8;
+  const k = 1;
+  const l = 1;
+  const m = 4;
+  const n = 1;
+
+  console.log([a, b, c, d, e, f, g, h, i, j, k, l, m, n].join(''));
+})();
---
+++ src/24.2.ts
@@ -0,0 +1,20 @@
+// ✎
+
+((): void => {
+  const a = 9;
+  const b = 6;
+  const c = 9;
+  const d = 2;
+  const e = 9;
+  const f = 9;
+  const g = 9;
+  const h = 4;
+  const i = 2;
+  const j = 9;
+  const k = 3;
+  const l = 9;
+  const m = 9;
+  const n = 6;
+
+  console.log([a, b, c, d, e, f, g, h, i, j, k, l, m, n].join(''));
+})();
